a49580de80deefe35e6387ca76b6d3a4f7ce873f
AntonioTorga <antonio.torga@ug.uchile.cl>
2025-12-18 18:54:50 -0300

    First commit of MM driver division

---
+++ melodies_monet/driver/__init__.py
@@ -0,0 +1,13 @@
+from .model import Model
+from .observation import Observation
+from .pair import Pair
+
+from .analysis import Analysis
+
+
+__all__ = (
+    "Pair",
+    "Observation",
+    "Model",
+    "Analysis",
+)
